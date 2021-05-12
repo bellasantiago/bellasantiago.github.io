c5a03b7de2d42b6f522ecdf80d66b90e1e32c5c1
Bella Santiago <bellasantiago.adl@gmail.com>
2021-05-12 15:44:58 +0930

    Project open in new tab

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -11,39 +11,45 @@ function Hero() {
                             <h2 className="mb-5">Recent Projects</h2>
                             <div className="row no-gutters">
                                 <div className="col-lg-6">
-                                    <a className="portfolio-item" href="https://bencyna.github.io/Catch-Source-Style/">
+                                    {/* eslint-disable-next-line */}
+                                    <a className="portfolio-item" href="https://bencyna.github.io/Catch-Source-Style/" target="_blank">
                                         <img className="img-fluid" src="img/portfolio-1.jpg" alt="" />
                                     </a>
                                 </div>
 
 
                                 <div className="col-lg-6">
-                                    <a className="portfolio-item" href="https://bellasantiago.github.io/day-scheduler/">
+                                    {/* eslint-disable-next-line */}
+                                    <a className="portfolio-item" href="https://bellasantiago.github.io/day-scheduler/" target="_blank">
                                         <img className="img-fluid" src="img/portfolio-3.jpg" alt="" />
                                     </a>
                                 </div>
 
                                 <div className="col-lg-6">
-                                    <a className="portfolio-item" href="https://arcane-waters-44478.herokuapp.com/">
+                                    {/* eslint-disable-next-line */}
+                                    <a className="portfolio-item" href="https://arcane-waters-44478.herokuapp.com/" target="_blank">
                                         <img className="img-fluid" src="img/portfolio-2.jpg" alt="" />
                                     </a>
                                 </div>
 
                                 <div className="col-lg-6">
-                                    <a className="portfolio-item" href="https://bellasantiago.github.io/hw-quiz/">
+                                    {/* eslint-disable-next-line */}
+                                    <a className="portfolio-item" href="https://bellasantiago.github.io/hw-quiz/" target="_blank">
                                         <img className="img-fluid" src="img/portfolio-4.jpg" alt="" />
                                     </a>
                                 </div>
 
                                 <div className="col-lg-6">
-                                    <a className="portfolio-item" href="https://github.com/bellasantiago/budget-tracker">
-                                        <img className="img-fluid" src="img/portfolio-6.jpg" alt="" />
+                                    {/* eslint-disable-next-line */}
+                                    <a className="portfolio-item" href="https://bellasantiago.github.io/weatherDashboard" target="_blank">
+                                        <img className="img-fluid" src="img/portfolio-5.png" alt="" />
                                     </a>
                                 </div>
 
                                 <div className="col-lg-6">
-                                    <a className="portfolio-item" href="https://bellasantiago.github.io/weatherDashboard">
-                                        <img className="img-fluid" src="img/portfolio-5.png" alt="" />
+                                    {/* eslint-disable-next-line */}
+                                    <a className="portfolio-item" href="https://github.com/bellasantiago/budget-tracker" target="_blank">
+                                        <img className="img-fluid" src="img/portfolio-6.png" alt="" />
                                     </a>
                                 </div>
                             </div>
